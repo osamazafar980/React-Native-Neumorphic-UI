e45679a31a7cf3c869e448228e80c1a8a996bd80
unknown <osamazafarmughal98@gmail.com>
2021-03-02 18:22:39 +0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer} from 'react-navigation'
+import ObjectRecogScreen from './screens/ObjectRecogScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const stackNavigator = createStackNavigator(
+  {
+    Home:ObjectRecogScreen
+  },{
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      headerTitleStyle: { alignSelf: 'center' },
+      title: "Blog Post App",
+    },
+    headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false,
+  }
+  }
+  
+);
+
+
+
+export default createAppContainer(stackNavigator);
